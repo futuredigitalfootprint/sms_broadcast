def15066428ea37229090565180e4b5fb92b07e6
TOMCAT\Matt Beck <matthew.beck@futuredigital.co.uk>
2017-10-26 15:38:46 +0100

    -Added full comments

--- form.php
+++ form.php
@@ -1,25 +1,25 @@
 <?php
 use Twilio\Rest\Client;
-use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\PHPMailer;								
 use PHPMailer\PHPMailer\Exception;
-require __DIR__ . '/Twilio/autoload.php';
-require __DIR__ . '/PHPMailer/src/Exception.php';
+require __DIR__ . '/Twilio/autoload.php';						//Include Twilio API for sms system
+require __DIR__ . '/PHPMailer/src/Exception.php';				//Include PHPMailer for mail system
 require __DIR__ . '/PHPMailer/src/PHPMailer.php';
 require __DIR__ . '/PHPMailer/src/SMTP.php';
-set_time_limit(0);
-$configContents  = file_get_contents('***');
-$configJson = json_decode($configContents);
-$sid = $configJson->ssid;
-$token = $configJson->token;
-$key = $configJson->pinkey;
-$numbers = $configJson->numbers;
-$emails = $configJson->email;
-$client = new Client($sid, $token);
-if(isset($_POST["submit"])){
-	$text = $_POST["message"];
-	$pin = $_POST["pin"];
-	if($pin==$key){
-		for($x = 0; $x < count($numbers); $x++){
+set_time_limit(0);												//Dont timeout if SMTP takes longer than 45 seconds to send
+$configContents  = file_get_contents('***');					//Json config file containing api keys and such
+$configJson = json_decode($configContents);						//Decode Json file into readable string
+$sid = $configJson->ssid;										//Twilio sid
+$token = $configJson->token;									//Twilio token api key
+$key = $configJson->pinkey;										//Pin to authorise sending message
+$numbers = $configJson->numbers;								//Array mobile numbers
+$emails = $configJson->email;									//Array of email addresses
+$client = new Client($sid, $token);								//Twilio sms client, bind sid and token
+if(isset($_POST["submit"])){									//If submit button clicked
+	$text = $_POST["message"];									//Store text from message box
+	$pin = $_POST["pin"];										//Store pin entered by user
+	if($pin==$key){												//If pin correct
+		for($x = 0; $x < count($numbers); $x++){				//Send sms for each number in numbers array
 			$client->messages->create(
 				$numbers[$x],
 				array(
@@ -28,28 +28,28 @@ if(isset($_POST["submit"])){
 					)
 				);
 		}
-		$mail = new PHPMailer(true);
-		$mail->isSMTP();
-		//$mail->SMTPDebug = 2;
-		//$mail->Debugoutput = 'html';
-		$mail->SMTPSecure = 'ssl';
-		$mail->Host = "smtp.gmail.com";
-		$mail->SMTPAuth = true;
-		$mail->Username = $configJson->username;
-		$mail->Password = $configJson->password;
-		$mail->Port = 465; 
-		$mail->From = $configJson->username;
-		$mail->FromName = "Alert warning system";
-		for($y = 0; $y < count($emails); $y++){
+		$mail = new PHPMailer(true);							//Init PHPMailer
+		$mail->isSMTP();										//Enable SMTP
+		//$mail->SMTPDebug = 2;									//Verbose logging
+		//$mail->Debugoutput = 'html';							//Html log format
+		$mail->SMTPSecure = 'ssl';								//SSL protocol, tsl is also available
+		$mail->Host = "smtp.gmail.com";							//Gmail host smtp server
+		$mail->SMTPAuth = true;									//Enable smtp authentication
+		$mail->Username = $configJson->username;				//Get username from json
+		$mail->Password = $configJson->password;				//Get password from json
+		$mail->Port = 465; 										//ssl port
+		$mail->From = $configJson->username;					//Use username for sender email
+		$mail->FromName = "Alert warning system";				//Sender name Default: Root User
+		for($y = 0; $y < count($emails); $y++){					//Add email addresses from emails array
 			$mail->addAddress($emails[$y]);
 		}
-		$mail->WordWrap = 50;
-		$mail->Subject = "Alert to All staff";
-		$mail->Body = $text;
-		if(!$mail->send()){
+		$mail->WordWrap = 50;									//Wraps after 50 chars
+		$mail->Subject = "Alert to All staff";					//Subject line
+		$mail->Body = $text;									//Use message text for the body of email
+		if(!$mail->send()){										//Send mail, thrwo error on failure
 			echo 'Mailer error: ' . $mail->ErrorInfo;
 		}		
-		header("Location:***");
+		header("Location:***");									//Redirect after sending
 		exit();
 	}
 }
