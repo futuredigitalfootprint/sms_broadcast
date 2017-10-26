bdaaa57cb4028acee2d80614290597bd806bf871
esqudo <matt.beck@virginmedia.com>
2017-10-26 12:14:37 +0100

    Update form.php

--- form.php
+++ form.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 require __DIR__ . '/Twilio/autoload.php';
 use Twilio\Rest\Client;
-$configContents  = file_get_contents('http://futuredigital.co.uk/form/config.json');
+$configContents  = file_get_contents('***');
 $configJson = json_decode($configContents);
 $sid = $configJson->ssid;
 $token = $configJson->token;
